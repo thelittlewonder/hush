72ddf3ecb26b87aef49ac72d7e849bffe3436022
Abhishek Sharma <hello.abhishek@outlook.com>
2020-06-05 13:07:31 +0530

    add pwa support

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener('fetch', function (event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
